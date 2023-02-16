c17c4a97ad828b9a96e66557fa1eacd13ef41077
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-02-16 17:11:06 +0100

    Añadimos axios al proyecto

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './css/style.css'
 import App from './App.vue'
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 createApp(App)
     .use(createPinia())
+    .use(VueAxios, axios)
     .mount('#app')
