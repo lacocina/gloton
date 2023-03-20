0d8f535b80626f73b7b92536c0ccc2912d8a0d34
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-03-20 17:02:13 +0100

    Pruebas antes de empezar el proyecto en base al diseño

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 .container.mx-auto.px-4.mt-8
   TheHeader
-  router-view
+  keep-alive
+    router-view
 </template>
 <script lang="ts" setup>
 import TheHeader from './components/header/TheHeader.vue'
--- src/pages/CategoriesList.vue
+++ src/pages/CategoriesList.vue
@@ -1,12 +1,82 @@
 <template lang="pug">
-div.bg-blue-500.p-4.text-white
-  | {{ $t('header.hello') }}
+.bg-white.p-4.text-white.flex.justify-between.items-center
+  .w-full
+    div
+      input(
+        type="text",
+        placeholder="Nombre*",
+        :class="[patata['nullInputStyles'], patata.title]"
+        )
+    div
+      input(
+        type="text",
+        placeholder="Descripción",
+        :class="patata['nullInputStyles']"
+        )
+  div
+    .flex.justify-between.items-center
+      input(
+        type="text",
+        placeholder="0",
+        :class="[patata['nullInputStyles'], patata.price]"
+        )
+      span(:class="patata.price") €
+.bg-white.p-4.text-white.flex.justify-between.items-center
+  .w-full
+    div
+      input(
+        type="text",
+        placeholder="Nombre*",
+        :class="[patata['nullInputStyles'], patata.title]"
+      )
+    div
+      input(
+        type="text",
+        placeholder="Descripción",
+        :class="patata['nullInputStyles']"
+      )
+  div
+    .flex.justify-between.items-center
+      input(
+        type="text",
+        placeholder="0",
+        :class="[patata['nullInputStyles'], patata.price]"
+      )
+      span(:class="patata.price") €
+
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
+<style lang="postcss" module="patata">
+.title {
+  font-weight: bold;
 
+  &::placeholder {
+    color: red;
+  }
+}
+
+.price {
+  font-weight: bold;
+  font-size: 1.5rem;
+  text-align: right;
+  &,
+  &::placeholder {
+    color: var(--color-primary);
+  }
+}
+
+.null-input-styles {
+  width: 100%;
+  &,
+  &:focus,
+  &:active {
+    border: none;
+    padding: 0;
+    --tw-ring-color: transparent;
+  }
+}
 </style>
