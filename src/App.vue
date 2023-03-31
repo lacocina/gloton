50fea33b8f251359341088473b721594cfad2176
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-04-01 01:39:52 +0200

    Ejemplos prácticos de CSS Modules con un componente o-box usando u propio module. EL scss del o-box trae un mapa de la capa settings

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template lang="pug">
 .container.mx-auto.px-4.mt-8
   TheHeader
-  div(:class="testclasses.oTest") o-test
-  div(:class="[testclasses.oTest,testclasses.oTestXs,testclasses.oTestMd]") o-test--xs
+  div(:class="testclasses.oStack") o-test
+  div(:class="testclasses['block__label']") label
+  div(:class="[testclasses.oStack,testclasses.oStackXs,testclasses.oStackMd]") o-test--xs
   input(type="checkbox", checked)
-  .class(:class="[testclasses.testclass, testclasses.caterina]") hello
-  div(:class="[testclasses.testclass, testclasses.caterina]") hello
+  div(:class="oBoxCss.oBox") o-box
+  div(:class="[oBoxCss.oBox, oBoxCss.xs]") o-box o-box-xs
+  div(:class="[oBoxCss.oBox, oBoxCss.sm]") o-box o-box-sm
+
+  o-box(size="sm") hello
+
+  // .class(:class="[testclasses.testclass, testclasses.caterina]") hello
+  // div(:class="[testclasses.testclass, testclasses.caterina]") hello
   router-view(v-slot="{ Component }")
     keep-alive
       component(:is="Component")
 </template>
 <script lang="ts" setup>
-// import styles from "@css/components/atoms/_index.scss";
-import testclasses from '@css/components/atoms/_indexx.module.scss'
+import oBoxCss from '@css/objects/o-box.module.css'
+import testclasses from '@css/objects/o-stack.module.css'
 import TheHeader from './components/header/TheHeader.vue'
+import OBox from "@components/OBox.vue";
 </script>
 
