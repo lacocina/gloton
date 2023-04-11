d7e65354016749962558138af68c06976ad3d0e9
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-04-11 16:53:28 +0200

    Creamos el componente card

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,33 @@
+<template>
+<div :class="[cardModule.card, oInsetModule.oInset]">
+    <div :class="cardModule.imageWrapper">
+        <img :src="imgSrc"/>
+    </div>
+    <div :class="[cardModule.content, oStackModule.oStack, oStackModule.xxs]">
+        <h3 :class="[txt.hero200, cardModule.title]">
+            Titulo
+        </h3>
+        <p :class="txt.subtitle100">Descripción</p>
+        <em :class="[txt.subtitle100, uTextAlignModule.right]">Ver todas las opciones</em>
+    </div>
+    <span v-if="false" class="material-symbols-rounded">
+        chevron_right
+    </span>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { getUrlPhoto } from "../../services/unsplash";
+import oInsetModule from '@css/objects/o-inset.module.css'
+import oStackModule from '@css/objects/o-stack.module.css'
+import cardModule from "@css/components/molecules/card.module.css"
+import txt from "@css/components/atoms/txt.module.css"
+import uTextAlignModule from "@css/utilities/u-text-align.module.css"
+
+const imgSrc = ref<String>('')
+async function getImage() {
+    imgSrc.value = await getUrlPhoto('nzyzAUsbV0M');
+}
+getImage();
+</script>
--- src/pages/CategoriesList.vue
+++ src/pages/CategoriesList.vue
@@ -4,15 +4,23 @@
   </template>
   <template v-else>
     <page-template :title="businessName">
-        contenido
+        <o-stack size="xs">
+          <h3 :class="txt.title200">Nuestra carta</h3>
+          <base-card/>
+          <base-card/>
+          <base-card/>
+        </o-stack>
     </page-template>
   </template>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
-import { useBusinessStore } from "../store/business";
-import PageTemplate from "@components/layout/PageTemplate.vue";
+import { useBusinessStore } from "../store/business"
+import PageTemplate from "@components/layout/PageTemplate.vue"
+import BaseCard from "@components/ui/BaseCard.vue"
+import OStack from "@components/objects/OStack.vue"
+import txt from "@css/components/atoms/txt.module.css"
 
 const businessStore = useBusinessStore()
 
