fafeb57f2241f89bdcf9942b6d8d86df1b3ad19d
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-08-04 17:44:03 +0200

    Añadimos los campos más básicos del login aunque no hagan nada aun. Estará pendiente de que enlacemos realmente la petición de usuario con la de los campos del cliente

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -1,14 +1,25 @@
 <template>
-<div>
-    <div v-if="adminStore.getUserLoading">Cargando...</div>
-    <base-button v-else @click="addUser">Añadir usuario</base-button>
-</div>
+<div v-if="adminStore.getUserLoading">Cargando...</div>
+<form v-else class="base-form" :class="oStack.oStack">
+    <div :class="BaseInputModule.baseInput">
+        <label for="name" :class="BaseInputModule.label">email label</label>
+        <input v-model.lazy="formConfig.email" id="email" :class="BaseInputModule.input" type="text" placeholder="-"/>
+    </div>
+    <div :class="BaseInputModule.baseInput">
+        <label for="lastname" :class="BaseInputModule.label">pass label</label>
+        <input v-model.lazy="formConfig.pass" id="pass" :class="BaseInputModule.input" type="password"/>
+    </div>
+    <base-button @click="addUser">Añadir usuario</base-button>
+</form>
 </template>
 
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
 import BaseButton from "@components/ui/BaseButton.vue";
+import oStack from '@css/objects/o-stack.module.css'
+import BaseInputModule from '@css/components/atoms/base-input.module.css'
 import { useAdminStore } from "@store/backoffice/admin.ts"
+import {reactive} from "vue";
 
 const adminStore = useAdminStore()
 const router = useRouter()
@@ -21,4 +32,10 @@ async function addUser() {
         await router.push({ path: '/admin' });
     }
 }
+
+const formConfig = reactive({
+    email: null,
+    pass: null
+})
+
 </script>
