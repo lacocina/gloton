2ab7dc57ad61b5203e110584cc56798cd53ee953
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-08-07 20:29:00 +0200

    admin: añadimos los items de la carta al detalle de la categoría

---
+++ src/components/admin/MenuItem.vue
@@ -0,0 +1,40 @@
+<template>
+<router-link to="admin" :class="[
+        adminMenuItemModule.adminMenuItem,
+        oFlexModule.oFlex,
+        item.show ? '' : adminMenuItemModule.deactivated,
+        oFlexModule.betweenCenter
+    ]">
+    <div :class="adminMenuItemModule.main">
+        <h3 :class="txt.title200">
+            {{ item.name }}
+        </h3>
+        <div :class="txt.subtitle100">
+            <b :class="adminMenuItemModule.price">{{ item.price }}€</b><em> - {{ item.description }}</em>
+        </div>
+    </div>
+    <div :class="[oFlexModule.oFlex, oFlexModule.betweenCenter]">
+        <span v-if="!item.show"
+              :class="adminMenuItemModule.deactivatedIcon"
+              class="material-symbols-rounded">
+            visibility_off
+        </span>
+        <span class="material-symbols-rounded">
+            keyboard_arrow_right
+        </span>
+    </div>
+</router-link>
+</template>
+
+<script lang="ts" setup>
+import type { MenuItem } from "@types/MenuItem.ts"
+import oFlexModule from "@css/objects/o-flex.module.css"
+import adminMenuItemModule from "@css/components/molecules/admin-menu-item.module.scss"
+import txt from "@css/components/atoms/txt.module.css"
+
+interface Props {
+    item: MenuItem
+}
+
+defineProps<Props>()
+</script>
--- src/pages/admin/AdminCategoryDetail.vue
+++ src/pages/admin/AdminCategoryDetail.vue
@@ -1,12 +1,28 @@
 <template>
 <page-template v-if="currentCategory" :title="currentCategory.name" back-button>
-    <o-stack size="lg">
+    <o-stack size="xl">
         <base-alert v-if="!currentCategory.items?.length" small>
             Esta categoría no aparecerá en la página porque aún no tiene ningún elemento
         </base-alert>
         <o-stack v-if="currentCategory.description">
             <h3 :class="txt.title200">Descripción</h3>
             <p>{{ currentCategory.description }}</p>
+        </o-stack>
+        <o-stack>
+            <div :class="[oFlexModule.oFlex, oFlexModule.betweenCenter]">
+                <h3 :class="txt.title200">
+                    {{ $t('admin.cards.productsNumber', { number: currentCategory.items?.length || 0 }) }}
+                </h3>
+                <div v-if="currentCategory.items">Ordenar</div>
+            </div>
+            <div v-if="currentCategory.items">
+                <menu-item v-for="item in currentCategory.items"
+                           :key="item.id"
+                           :item="item"/>
+            </div>
+            <div>
+                Añadir otro item
+            </div>
         </o-stack>
     </o-stack>
 </page-template>
@@ -20,7 +36,9 @@ import type { MenuCategory } from "@types/MenuCategory.ts"
 import PageTemplate from "@components/layout/PageTemplate.vue"
 import OStack from "@components/objects/OStack.vue"
 import txt from "@css/components/atoms/txt.module.css"
-import BaseAlert from "@components/ui/BaseAlert.vue";
+import BaseAlert from "@components/ui/BaseAlert.vue"
+import oFlexModule from "@css/objects/o-flex.module.css"
+import MenuItem from "@components/admin/MenuItem.vue"
 
 const adminStore = useAdminStore()
 const route = useRoute()
