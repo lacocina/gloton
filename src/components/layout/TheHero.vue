03b4579d406370f3709b516321398fe6bed5aa76
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-08-08 13:03:15 +0200

    Evitamos el error de la imgSrc del hero pintandolo cuando ya ha devuelto la imagen

--- src/components/layout/TheHero.vue
+++ src/components/layout/TheHero.vue
@@ -1,14 +1,14 @@
 <template>
-<section :class="heroModule.hero">
-  <div :class="heroModule.imgWrapper">
+<section :class="hero.hero">
+  <div v-if="imgSrc" :class="hero.imgWrapper">
       <img :src="imgSrc" :alt="title">
   </div>
-  <div v-if="backButton" :class="heroModule.nav">
-    <router-link :to="backPath" :class="heroModule.backButton" class="material-symbols-rounded">
+  <div v-if="backButton" :class="hero.nav">
+    <router-link :to="backPath" :class="hero.backButton" class="material-symbols-rounded">
         arrow_back_ios_new
     </router-link>
   </div>
-  <div :class="heroModule.main">
+  <div :class="hero.main">
     <h1 :class="txt.hero400">{{ title }}</h1>
     <h2 :class="txt.subtitle200">{{ subtitle }}</h2>
   </div>
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts" setup>
-import heroModule from "@css/components/molecules/hero.module.css"
+import hero from "@css/components/molecules/hero.module.css"
 import txt from "@css/components/atoms/txt.module.css"
 import { useRouter } from "vue-router";
 
@@ -25,7 +25,7 @@ const router = useRouter()
 interface Props {
   title: string,
   subtitle?: string,
-  imgSrc: string,
+  imgSrc?: string | undefined,
   backButton?: boolean
 }
 
