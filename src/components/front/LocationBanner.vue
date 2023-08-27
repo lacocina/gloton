ba2afe34d55543a3262d027b6334e7d9a88bf84e
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-08-27 14:01:49 +0200

    Añadimos el banner de dirección en la home

---
+++ src/components/front/LocationBanner.vue
@@ -0,0 +1,58 @@
+<template>
+<section :class="locationBanner.locationBanner">
+  <div :class="locationBanner.imgWrapper">
+    <img v-if="imgSrc" :src="imgSrc" alt="Nuestro local">
+  </div>
+  <o-stack size="xs">
+    <h3 :class="txt.title200">¿Dónde estamos?</h3>
+    <address>
+      C/ Bernat Calvet, 2
+      <br>
+      Mallorca
+      <br>
+      España
+    </address>
+    <div>
+      <base-button button-style="secondary" button-size="sm">
+        Google maps
+      </base-button>
+    </div>
+  </o-stack>
+</section>
+</template>
+
+<script setup lang="ts">
+import OStack from "@components/objects/OStack.vue"
+import txt from "@css/components/atoms/txt.module.css"
+import BaseButton from "@components/ui/BaseButton.vue"
+import { getUrlPhoto } from "../../services/unsplash.ts"
+import { computedAsync } from "@vueuse/core"
+
+const imgSrc = computedAsync(() => getUrlPhoto('iBR8QeaYNbw'))
+</script>
+
+<style lang="scss" module="locationBanner">
+.location-banner {
+  display: grid;
+  align-items: end;
+  grid-template-columns: 1fr 1fr;
+  composes: lg from '../../css/utilities/u-gap.module.css';
+
+  address {
+    font-size: var(--font-size-100);
+    font-style: italic;
+    color: var(--color-gray-300);
+  }
+}
+
+.img-wrapper {
+  height: 12rem;
+  overflow: hidden;
+  border-radius: var(--border-radius-lg);
+
+  img {
+    object-fit: cover;
+    object-position: center center;
+  }
+}
+</style>
--- src/pages/CategoriesList.vue
+++ src/pages/CategoriesList.vue
@@ -5,19 +5,22 @@
   <template v-else>
 <!--    <vue3-lottie :animationLink="loadingJSON" :height="200" :width="200" />-->
     <page-template :title="businessName" :img-src="imgSrc">
-        <o-stack size="xs">
-            <h3 :class="txt.title200">Nuestra carta</h3>
-            <template v-if="menuCategories">
-              <base-card v-for="category in menuCategories"
-                         :key="category.id"
-                         :href="'/detail/'+ category.id"
-                         :img-src="categoryImgSrc"
-                         :title="category.name"
-                         :description="category.description"
-                         action-text="Ver todas las opciones"
-              />
-            </template>
-            <span v-else>No hay ninguna categoría visible en la carta en estos momentos</span>
+        <o-stack size="xxl">
+          <o-stack size="xs">
+              <h3 :class="txt.title200">Nuestra carta</h3>
+              <template v-if="menuCategories">
+                <base-card v-for="category in menuCategories"
+                           :key="category.id"
+                           :href="'/detail/'+ category.id"
+                           :img-src="categoryImgSrc"
+                           :title="category.name"
+                           :description="category.description"
+                           action-text="Ver todas las opciones"
+                />
+              </template>
+              <span v-else>No hay ninguna categoría visible en la carta en estos momentos</span>
+          </o-stack>
+          <location-banner/>
         </o-stack>
     </page-template>
   </template>
@@ -33,6 +36,7 @@ import PageTemplate from "@components/layout/PageTemplate.vue"
 import BaseCard from "@components/ui/BaseCard.vue"
 import OStack from "@components/objects/OStack.vue"
 import txt from "@css/components/atoms/txt.module.css"
+import LocationBanner from "@components/front/LocationBanner.vue";
 
 // TODO - Lottie
 // import Vue3Lottie from "vue3-lottie";
