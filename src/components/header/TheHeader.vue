d77f24052ce2bedf0130f43660eb1e259951246c
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-09-06 12:02:58 +0200

    Ocultamos el cambio de idioma

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -5,22 +5,12 @@
     <the-header-item href="/admin" text="Admin"/>
     <the-header-item href="/css" text="CSS Docs"/>
   </nav>
-
-  <select v-model="$i18n.locale">
-    <option value="es-ES">
-      es
-    </option>
-    <option value="en-US">
-      en
-    </option>
-  </select>
-
   <light-dark-mode/>
 </header>
 </template>
 
 <script setup>
 import TheHeaderModule from "@css/components/molecules/the-header.module.css"
-import TheHeaderItem from "./TheHeaderItem.vue";
-import LightDarkMode from "@components/header/LightDarkMode.vue";
+import TheHeaderItem from "./TheHeaderItem.vue"
+import LightDarkMode from "@components/header/LightDarkMode.vue"
 </script>
