eb0c5c27f88376388a55e81cb8d30fab031932ef
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-09-06 12:02:47 +0200

    fix

--- src/components/layout/PageTemplate.vue
+++ src/components/layout/PageTemplate.vue
@@ -11,8 +11,6 @@
 </template>
 
 <script lang="ts" setup>
-import { getUrlPhoto } from "../../services/unsplash";
-import { computedAsync } from "@vueuse/core";
 import contentPage from "@css/components/molecules/content-page.module.css"
 import TheFooter from "@components/layout/TheFooter.vue";
 import TheHero from "@components/layout/TheHero.vue";
