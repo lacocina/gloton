05813736e1e74e6ce13daf721fcc14f8ad9f96fa
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2023-09-07 18:55:50 +0200

    Validación del formulario de datos de business, pendiente hacer que el modal salga después de la validación del formulario

--- src/pages/admin/AdminBusiness.vue
+++ src/pages/admin/AdminBusiness.vue
@@ -4,17 +4,50 @@
                  title="Cambio de datos"
                  text="Estás a punto de cambiar los datos públicos de tu negocio ¿Estás seguro?"/>
     <form @submit.prevent="$refs.changeNameDialog.open()" class="base-form" :class="oStack.oStack">
-        <div :class="BaseInputModule.baseInput">
-            <label for="name" :class="BaseInputModule.label">name label</label>
-            <input v-model.lazy="formConfig.name" id="name" :class="BaseInputModule.input" type="text" placeholder="-"/>
+        <div :class="baseInput.baseInput">
+          <label for="name" :class="baseInput.label">name label</label>
+          <input v-model.lazy="formConfig.name"
+                 @blur="validateName"
+                 id="name"
+                 :class="[
+                     baseInput.input,
+                     nameError ? baseInput.error : ''
+                 ]"
+                 type="text"
+                 placeholder="-"/>
+          <p v-if="nameError" :class="baseInput.errorMessage">
+            *Este campo no puede estar vacío
+          </p>
         </div>
-        <div :class="BaseInputModule.baseInput">
-            <label for="email" :class="BaseInputModule.label">email label</label>
-            <input v-model.lazy="formConfig.email" id="email" :class="BaseInputModule.input" type="text" placeholder="-"/>
+        <div :class="baseInput.baseInput">
+          <label for="email" :class="baseInput.label">email label</label>
+          <input v-model.lazy="formConfig.email"
+                 @blur="validateEmail"
+                 id="email"
+                 :class="[
+                     baseInput.input,
+                     emailError ? baseInput.error : ''
+                 ]"
+                 type="text"
+                 placeholder="-"/>
+          <p v-if="emailError" :class="baseInput.errorMessage">
+            *Este campo no puede estar vacío
+          </p>
         </div>
-        <div :class="BaseInputModule.baseInput">
-            <label for="phoneNumber" :class="BaseInputModule.label">phoneNumber label</label>
-            <input v-model.lazy="formConfig.phoneNumber" id="phoneNumber" :class="BaseInputModule.input" type="text" placeholder="-"/>
+        <div :class="baseInput.baseInput">
+          <label for="phoneNumber" :class="baseInput.label">phoneNumber label</label>
+          <input v-model.lazy="formConfig.phoneNumber"
+                 @blur="validatePhoneNumber"
+                 id="phoneNumber"
+                 :class="[
+                       baseInput.input,
+                       phoneNumberError ? baseInput.error : ''
+                   ]"
+                 type="text"
+                 placeholder="-"/>
+          <p v-if="phoneNumberError" :class="baseInput.errorMessage">
+            *Este campo no puede estar vacío
+          </p>
         </div>
         <div :class="[oFlex.endCenter, uGap.md]">
           <base-button button-style="secondary" @click="cancel">Cancelar</base-button>
@@ -24,17 +57,17 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, reactive } from "vue"
+import { onMounted, reactive, ref } from "vue"
 import { useAdminStore } from "@store/backoffice/admin.ts"
 import oStack from '@css/objects/o-stack.module.css'
-import BaseInputModule from '@css/components/atoms/base-input.module.css'
+import baseInput from '@css/components/atoms/base-input.module.css'
 import type { BusinessForm } from "@types/BusinessForm.ts"
 import oFlex from "@css/objects/o-flex.module.css"
 import uGap from "@css/utilities/u-gap.module.css"
 import BaseButton from "@components/ui/BaseButton.vue"
 import { useNotification } from "@kyvg/vue3-notification"
 import { useRouter } from "vue-router"
-import BaseDialog from "@components/ui/BaseDialog.vue";
+import BaseDialog from "@components/ui/BaseDialog.vue"
 
 const adminStore = useAdminStore()
 const { notify } = useNotification()
@@ -52,19 +85,48 @@ onMounted(() => {
   formConfig.phoneNumber = adminStore.business.phoneNumber
 })
 
+const nameError = ref(false)
+const emailError = ref(false)
+const phoneNumberError = ref(false)
+
+function validateName() {
+  nameError.value = !(!!formConfig.name)
+}
+
+function validateEmail() {
+  emailError.value = !(!!formConfig.email)
+}
+
+function validatePhoneNumber() {
+  phoneNumberError.value = !(!!formConfig.phoneNumber)
+}
+
 async function updateData() {
-  try {
-    await adminStore.updateBusiness(formConfig)
-    notify({
-      type: 'success',
-      title: 'Cambio realizado con éxito'
-    })
-    router.back()
-  } catch {
+  // TODO - Hacer que primero compruebe esto y luego abra el modal
+  if (
+      !nameError.value
+      && !emailError.value
+      && !phoneNumberError.value
+  ) {
+    try {
+      await adminStore.updateBusiness(formConfig)
+      notify({
+        type: 'success',
+        title: 'Cambio realizado con éxito'
+      })
+      router.back()
+    } catch {
+      notify({
+        type: 'error',
+        title: 'Ha habido algún error',
+        text: 'Por favor, vuelve a intentarlo más tarde'
+      })
+    }
+  } else {
     notify({
       type: 'error',
-      title: 'Ha habido algún error',
-      text: 'Por favor, vuelve a intentarlo más tarde'
+      title: 'Formulario incorrecto',
+      text: 'Algún campo del formulario no cumple las reglas. Por favor, revísalo mejor.'
     })
   }
 }
